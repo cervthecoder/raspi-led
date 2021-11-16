66f17df8f01e9186bb052a59d099f95d46bd78df
cervthecoder <matej.cervenka03@gmail.com>
2021-11-16 22:38:40 +0100

    Make changes in main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
+    print(args)
     args = [str(arg) for arg in args]
     args1 = [arg+", " for arg in args[1:-1]]
     args = args1 + [args[-1]]
+    print(args)
     try:
         if args[0] in modes.methods():
             eval(f"modes.{args[0]}("".join({args[1:]}))")
