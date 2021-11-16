f581f953c1d9d65292fcf5b746d856ec7e1dbf66
cervthecoder <matej.cervenka03@gmail.com>
2021-11-16 22:29:21 +0100

    Make changes in main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     args = [str(arg) for arg in args]
+    args1 = [arg+", " for arg in args[1:-1]]
+    args = args1 + [args[-1]]
     try:
         if args[0] in modes.methods:
             eval(f"modes.{args[0]}("".join({args[1:]}))")
         elif args[0]=="help":
             print("help message")
     except:
-        print("An error occurred try raspi-led -h")
+        raise(ValueError)
