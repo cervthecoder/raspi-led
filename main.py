860c0b3f92516f740d4add1a9b6bd19ecf7cafe1
cervthecoder <matej.cervenka03@gmail.com>
2021-11-17 12:10:06 +0100

    Make changes in main.py

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ if __name__ == '__main__':
     args = [args[1]] + args1 + [args[-1]]
     print(args)
     try:
-        if args[0] in modes.methods():
-            arg = ''.join(args[1:])
-            print(f"modes.{args[0]}({arg})")
-            eval(f"modes.{args[0]}({arg})")
+        if args[0] != args[1]:
+            if args[0] in modes.methods():
+                arg = ''.join(args[1:])
+                print(f"modes.{args[0]}({arg})")
+                eval(f"modes.{args[0]}({arg})")
+        elif args[0] == args[1]:
+            eval(f"modes.{args[0]}()")
         elif args[0]=="help":
             print("help message")
     except:
-        raise ValueError
+        print(ValueError)
