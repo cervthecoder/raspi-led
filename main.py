02c063058ba636892f5efd21f45b362353b1a8b2
cervthecoder <matej.cervenka03@gmail.com>
2021-11-17 13:27:33 +0100

    Make changes in main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             args = sys.argv
             args = [str(arg) for arg in args]
             color = args[2]
-            print(f"modes.simple_color(color, args[3])")
+            print(f"modes.simple_color(color, {args[3]})")
             eval(f"modes.simple_color(color, args[3])", {"color":color})
         if args[0] in modes.methods():
             arg = ''.join(args[1:])
